d3a508f5105accef4e1c08d067633dbbcd4fdfeb
alvarovp27 <avalenciaparra@gmail.com>
2019-10-09 15:03:30 +0200

    refactor: added drools library for testing purposes

--- src/main/java/es/us/idea/dmn4spark/Main.scala
+++ src/main/java/es/us/idea/dmn4spark/Main.scala
@@ -1,7 +1,21 @@
 package es.us.idea.dmn4spark
 
+import es.us.idea.spark4drools
+import org.apache.spark.sql.SparkSession
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world")
+    val spark = SparkSession.builder().master("local").appName("Sample").getOrCreate()
+
+    import spark4drools.implicits._
+    import implicits._
+    import spark.implicits._
+
+    val df = spark.read.option("header", "true").csv("input/movies_dataset_enriched.csv")
+      .drools.fromLocalPath("input/moviesDatasetBR.drl")//.filter($"BR3" === false)
+      .dmn.loadFromLocalPath("models/simulation(7).dmn")
+
+    df.printSchema
+    df.show()
   }
 }
